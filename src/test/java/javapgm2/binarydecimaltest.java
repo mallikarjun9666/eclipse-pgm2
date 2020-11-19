778a2f252635f0da4548ea3f37c401b9ab7ff475
arjun <arjun123@gmail.com>
2020-11-19 16:45:33 +0530

    commit java pgm2

---
+++ src/test/java/javapgm2/binarydecimaltest.java
@@ -0,0 +1,14 @@
+package javapgm2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class binarydecimaltest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
